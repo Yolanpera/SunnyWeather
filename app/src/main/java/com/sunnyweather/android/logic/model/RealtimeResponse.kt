bc799557a6e009bd30c316dbea4dc8cc8de6e42b
yanpeng.p <yanpeng.p@bytedance.com>
2020-10-03 00:43:28 +0800

    Weather Models

--- app/src/main/java/com/sunnyweather/android/logic/model/RealtimeResponse.kt
+++ app/src/main/java/com/sunnyweather/android/logic/model/RealtimeResponse.kt
@@ -3,7 +3,7 @@ package com.sunnyweather.android.logic.model
 import com.google.gson.annotations.SerializedName
 
 //将所有的数据类型定义在内部，避免出现和其他接口的数据类型有同名的冲突
-data class RealtimeResponse(val statu: String, val result: Result){
+data class RealtimeResponse(val status: String, val result: Result){
     data class Result(val realtime: RealTime)
 
     data class RealTime(val skycon: String, val temperature: Float, @SerializedName("air_quality")val airQuality: AirQuality)
